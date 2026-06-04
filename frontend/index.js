9503fed40962f6972040199e1b8c954b68a63796
Andrej <andrej@vitech.dev>
2026-06-04 13:40:46 +0200

    test blue-green deployment 3

--- frontend/index.js
+++ frontend/index.js
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
         <h1>Frontend Application</h1>
         <hr>
         <div style="background: #f4f4f4; padding: 20px; display: inline-block; border-radius: 10px;">
-            <h3 style="color: blue;">Version 1!</h3>
+            <h3 style="color: blue;">Version 1 Active!</h3>
             <p><b>Message from Backend:</b> ${data.message}</p>
             <p><b>Service Status:</b> ${data.status}</p>
         </div>
